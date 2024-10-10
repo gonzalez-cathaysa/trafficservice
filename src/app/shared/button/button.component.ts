af96e51495750be422da89334c92b189d06a364f
gonzalez-cathaysa <cathay.gonzalez@gmail.com>
2024-10-10 17:16:53 +0200

    Extending Built-in Elements with Custom Components via Attribute Selectors

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  //element selector Button app-button
+  //attribute selector This is just how in CSS you would set up
+  selector: 'button[appButton]',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+}
