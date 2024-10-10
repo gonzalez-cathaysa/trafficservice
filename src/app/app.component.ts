43108a91bf89cb2b51ca1444b5d2eef6e23f403e
gonzalez-cathaysa <cathay.gonzalez@gmail.com>
2024-10-10 21:16:50 +0200

    Exploring advanced content projection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
 import { TrafficComponent } from './dashboard/traffic/traffic.component';
 import { TicketsComponent } from './dashboard/tickets/tickets.component';
-import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +15,7 @@ import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-ite
     TrafficComponent,
     TicketsComponent,
     DashboardItemComponent
-],
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
