a5ef08a85bd8a3db7b4fb73e7d01bd0ad9e24b59
gonzalez-cathaysa <cathay.gonzalez@gmail.com>
2024-10-14 15:10:14 +0200

    Interacting with Host Elements via @HostBinding

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -9,10 +9,25 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
   //Host wants an object as a value, and that object takes key value pairs of your choice
   //So here I could add a key named class
-  // host: {
-  //   class: 'control'
-  // }
+  host: {
+    class: 'control',
+    //'(click)': 'onClick()'
+  }
 })
 export class ControlComponent {
+
+  //It will take a look at this property name, and it will then add it as a property
+  //It is discourage you should use, in the past it was a common way of setting those host properties
+  // @HostListener()
+  // @HostBinding('class') className = 'control';
+
+  @HostListener('click') onClick() {
+    console.log('Clicked');
+  }
+
   label = input.required<string>();
+
+  // onClick(){
+  //   console.log('Clicked');
+  // }
 }
