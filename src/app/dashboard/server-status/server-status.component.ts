6e53e17b103fe6826e40ddfe60bc15f9b0c12b77
gonzalez-cathaysa <cathay.gonzalez@gmail.com>
2024-10-14 23:08:14 +0200

    Content cleanup with DestroyRef

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -9,11 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerStatusComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  // private interval?: ReturnType<typeof setInterval>;
+  private destroyRef = inject(DestroyRef);
+
   constructor() {}
 
   ngOnInit() {
-    setInterval(() => {
-      const rnd = Math.random(); //0-0.99999
+    console.log('ON INIT');
+    const interval = setInterval(() => {
+      const rnd = Math.random(); // 0 - 0.99999999999999
+
       if (rnd < 0.5) {
         this.currentStatus = 'online';
       } else if (rnd < 0.9) {
@@ -22,5 +27,16 @@ export class ServerStatusComponent implements OnInit {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
   }
+
+  ngAfterViewInit() {
+    console.log('after view init');
+  }
+
+  // ngOnDestroy() {
+  //   clearInterval(this.interval);
+  // }
 }
